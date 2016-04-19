6ba5a99617ab9acb83a308e9bbb0ac08b8ca38fe
Russ Henmi <rksjnet@gmail.com>
2016-04-19 14:21:27 -0700

    Random insult generator

---
+++ Insult.java
@@ -0,0 +1,77 @@
+import java.util.Random; 
+
+class Insult {
+
+public static void main(String args[]) {
+
+String[] noun = {
+"pilot",
+"canoe",
+"captain",
+"pirate",
+"hammer",
+"knob",
+"box",
+"jockey",
+"nazi",
+"waffle",
+"goblin",
+"blossom",
+"biscuit",
+"clown",
+"socket",
+"monster",
+"hound",
+"dragon",
+"balloon"};
+
+String[] adj1 = {
+"lazy",
+"stupid",
+"insecure",
+"idiotic",
+"slimy",
+"slutty",
+"smelly",
+"pompus",
+"communist",
+"pie-eating",
+"racist",
+"elitist",
+"white-trash",
+"butterface",
+"tone-deaf",
+"ugly",
+"creepy" };
+
+String[] adj2 = {
+"douche",
+"ass",
+"turd",
+"rectum",
+"butt",
+"crotch",
+"bitch",
+"turd",
+"nut" };
+
+int random1, random2, random3;
+
+Random randomGenerator = new Random();
+
+random1 = randomGenerator.nextInt(1000);
+random2 = randomGenerator.nextInt(1000);
+random3 = randomGenerator.nextInt(1000);
+
+int length1 = adj1.length;
+int length2 = adj2.length;
+int length3 = noun.length;
+
+int index1 = random1 % length1;
+int index2 = random1 % length2;
+int index3 = random1 % length3;
+
+
+System.out.println ("You are a " + adj1[index1] + " " + adj2[index2] + " " + noun[index3] + "!");
+}
+}
